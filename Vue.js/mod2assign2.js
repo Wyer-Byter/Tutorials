1a393df557787fa15cceef951e9aecba12cdedbb
Wyer-Byter <wyerbyter@gmail.com>
2018-06-28 17:28:52 -0400

    Finished Module 2 Assignment 2

---
+++ Vue.js/mod2assign2.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#exercise',
+    data: {
+        value: ''
+    },
+    methods: {
+        alertMe: function() {
+            alert('Alert!');
+        },
+        storeValue: function(event) {
+            this.value = event.target.value
+        }
+    }
+
+});
