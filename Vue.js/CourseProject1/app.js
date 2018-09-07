a92ff1f7d154054153ec6c56bbf53ceb3c71d1a9
Wyer-Byter <wyerbyter@gmail.com>
2018-09-07 16:06:08 -0400

    Working on first project

---
+++ Vue.js/CourseProject1/app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+        gameStarted: false,
+        healthYou: 100,
+        healthMonster: 100
+    },
+    methods: {
+        attack: function() {
+            
+        }
+    }
+})
