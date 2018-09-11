ac21a9e734da8e36179a07c8e11dc5db63593cf2
Wyer-Byter <wyerbyter@gmail.com>
2018-09-11 16:24:08 -0400

    Halfway through project

--- Vue.js/CourseProject1/app.js
+++ Vue.js/CourseProject1/app.js
@@ -3,11 +3,35 @@ new Vue({
     data: {
         gameStarted: false,
         healthYou: 100,
-        healthMonster: 100
+        healthMonster: 100,
+        maxAttack: 15,
+        maxSpecialAttack: 30,
+        maxHeal: 15,
+        attackLog: []
     },
     methods: {
         attack: function() {
-            
+            var yourAttack = this.randomGen(this.maxAttack);
+            var monsterAttack = this.randomGen(this.maxAttack);
+
+            this.healthMonster = this.healthMonster - yourAttack;
+            this.healthYou = this.healthYou - monsterAttack;
+        },
+        specialAttack: function() {
+            var yourAttack = this.randomGen(this.maxSpecialAttack);
+            var monsterAttack = this.randomGen(this.maxAttack);
+
+            this.healthMonster = this.healthMonster - yourAttack;
+            this.healthYou = this.healthYou - monsterAttack;
+        },
+        heal: function() {
+            var yourHeal = this.randomGen(this.maxAttack);
+            var monsterAttack = this.randomGen(this.maxAttack);
+
+            this.healthYou = this.healthYou - monsterAttack + yourHeal;
+        },
+        randomGen: function(maxNumber) {
+            return Math.floor(Math.random() * (maxNumber + 1));
         }
     }
 })
