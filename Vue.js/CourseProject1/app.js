f73da95824a33dc4f2adefc226316c0e100cd466
Jonathan Stephenson <wyerbyter@gmail.com>
2018-09-15 13:30:15 -0400

    Completed first project

--- Vue.js/CourseProject1/app.js
+++ Vue.js/CourseProject1/app.js
@@ -2,36 +2,90 @@ new Vue({
     el: '#app',
     data: {
         gameStarted: false,
+        healthMax: 100,
         healthYou: 100,
         healthMonster: 100,
         maxAttack: 15,
         maxSpecialAttack: 30,
         maxHeal: 15,
-        attackLog: []
+        maxMonsterAttack: 20,
+        minAttack: 2,
+        minSpecialAttack: 15,
+        minMonsterAttack: 3,
+        attackLogs: []
     },
     methods: {
         attack: function() {
-            var yourAttack = this.randomGen(this.maxAttack);
-            var monsterAttack = this.randomGen(this.maxAttack);
+            var yourAttack = this.randomGen(this.minAttack, this.maxAttack);
+            this.healthMonster -= yourAttack;
+            this.attackLogs.unshift({isPlayer: true, text: "Player hits Monster for " + yourAttack});
+            if (this.hasWon()) {
+                return;
+            }
 
-            this.healthMonster = this.healthMonster - yourAttack;
-            this.healthYou = this.healthYou - monsterAttack;
+            this.monsterAttack();
         },
         specialAttack: function() {
-            var yourAttack = this.randomGen(this.maxSpecialAttack);
-            var monsterAttack = this.randomGen(this.maxAttack);
-
-            this.healthMonster = this.healthMonster - yourAttack;
-            this.healthYou = this.healthYou - monsterAttack;
+            var yourAttack = this.randomGen(this.minSpecialAttack, this.maxSpecialAttack);
+            this.healthMonster -= yourAttack;
+            this.attackLogs.unshift({isPlayer: true, text: "Player hits Monster hard for " + yourAttack});
+            if (this.hasWon()) {
+                return;
+            }
+            
+            this.monsterAttack();
         },
         heal: function() {
-            var yourHeal = this.randomGen(this.maxAttack);
-            var monsterAttack = this.randomGen(this.maxAttack);
+            if (this.healthYou < this.healthMax - this.maxHeal) {
+                this.healthYou += this.maxHeal;
+            } else {
+                this.healthYou = this.healthMax;
+            }
+            this.attackLogs.unshift({isPlayer: true, text: "Player healse for " + this.maxHeal});
 
-            this.healthYou = this.healthYou - monsterAttack + yourHeal;
+            this.monsterAttack();
         },
-        randomGen: function(maxNumber) {
-            return Math.floor(Math.random() * (maxNumber + 1));
+        randomGen: function(minNumber, maxNumber) {
+            var range = (maxNumber - minNumber) + 1
+            return Math.floor((Math.random() * range) + minNumber);
+        },
+        monsterAttack: function() {
+            var damage = this.randomGen(this.minMonsterAttack, this.maxMonsterAttack);
+            this.healthYou -= damage;
+            this.attackLogs.unshift({isPlayer: false, text: "Monster hits Player for " + damage});
+            if (this.hasWon()) {
+                return;
+            }
+        },
+        startGame: function() {
+            this.gameStarted = true;
+            this.healthYou = this.healthMax;
+            this.healthMonster = this.healthMax;
+            this.attackLogs = [];
+        },
+        giveUp: function() {
+            this.gameStarted = false;
+        },
+        hasWon: function() {
+            if (this.healthMonster <= 0) {
+                if (confirm("You won! New game?")) {
+                    this.startGame();  
+                } else {
+                    this.gameStarted = false;
+                }
+
+                return true;
+            } else if (this.healthYou <= 0) {
+                if (confirm("You lost! New game?")) {
+                    this.startGame();
+                } else {
+                    this.gameStarted = false;
+                }
+
+                return true;
+            }
+            
+            return false;
         }
     }
 })
