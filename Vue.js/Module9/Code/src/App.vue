3b6ce8327c0a60f1e0426ca35051d5c6824898aa
Wyer-Byter <wyerbyter@gmail.com>
2018-10-25 15:54:58 -0400

    Finished with slots Module 9

--- Vue.js/Module9/Code/src/App.vue
+++ Vue.js/Module9/Code/src/App.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12">
                <app-quote>
-                   <h2>{{ quoteTitle }}</h2>
+                   <h2 slot="title">{{ quoteTitle }}</h2>
                    <p>A wonderful Quote</p>
                </app-quote>
             </div>
